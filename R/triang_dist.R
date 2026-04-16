c3a7b14c32650055a23f085148e133825a8fde3b
Irene Ponce Huerta <iponhuer@alumni.uv.es>
2026-04-16 20:53:30 +0200

    all functions

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -17,5 +17,42 @@
 if (any(min > max)) stop("min cannot be greater than max")
 if (any(mode < min | mode > max)) stop("mode must be between min and max")
 
+res <- numeric(length(x))
+
+
+
+}
+
+
+
+#' @rdname triangular
+#' @export
+ptriang <- function(x, min, max, mode) {
+  if (any(min > max)) stop("min cannot be greater than max")
+  if (any(mode < min | mode > max)) stop("mode must be between min and max")
+
+}
+
+
+
+
+
+#' @rdname triangular
+#' @export
+qtriang <- function(x, min, max, mode) {
+  if (any(min > max)) stop("min cannot be greater than max")
+  if (any(mode < min | mode > max)) stop("mode must be between min and max")
+  if (any(x < 0 | x > 1)) stop("p must be between 0 and 1")
+
+
+
+}
+
+
+
+
+#' @rdname triangular
+#' @export
+rtriang <- function(n, min, max, mode){
 
 }
