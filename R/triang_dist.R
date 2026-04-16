4bb989b33785a089c42163b0252accb5cbb07503
Irene Ponce Huerta <iponhuer@alumni.uv.es>
2026-04-16 20:42:16 +0200

    params description and name

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -1,1 +1,13 @@
 #' @title Triangular Distribution
+#' @description Density, distribution function, quantile function and random generation
+#' for the triangular distribution.
+#'
+#' @param x,q Vector of quantiles.
+#' @param p Vector of probabilities.
+#' @param n Number of observations.
+#' @param min Lower limit (a).
+#' @param max Upper limit (b).
+#' @param mode Mode (c).
+#' @name triangular
+#'
+#'
